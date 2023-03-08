fd5e376a1973fb4db2fc8bef3a50a382cc8e1db3
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2023-03-08 16:34:18 +0300

    models and admin site

--- stock/models.py
+++ stock/models.py
@@ -44,12 +44,20 @@ class Purchases(models.Model):
     price=models.DecimalField(max_digits=6,decimal_places=2)
     price_total=models.DecimalField(max_digits=8,decimal_places=2,blank=True)
     
+    def __str__(self):
+        return f"{self.product} - {self.quantity}"
+    
 class Sales(models.Model):
     user=models.ForeignKey(User,on_delete=models.SET_NULL, null=True)
     brand=models.ForeignKey(Brand,on_delete=models.SET_NULL,null=True,related_name="b_sales")
     product=models.ForeignKey(Product,on_delete=models.CASCADE,related_name="sale")
     quantity=models.PositiveSmallIntegerField()
     price=models.DecimalField(max_digits=6,decimal_places=2)
-    price_total=models.DecimalField(max_digits=8,decimal_places=2,blank=True)    
+    price_total=models.DecimalField(max_digits=8,decimal_places=2,blank=True)  
+    
+    def __str__(self):
+        return f"{self.product} - {self.quantity}"
+    
+      
         
     
