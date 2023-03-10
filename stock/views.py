63d1b405f8b6a61f5e6ea9a25f3d0453620a4a08
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2023-03-10 10:50:07 +0300

    Update Purchases

--- stock/views.py
+++ stock/views.py
@@ -58,6 +58,7 @@ class PurchasesView(viewsets.ModelViewSet):
         purchase=request.data
         product=Product.objects.get(id=purchase["product_id"])
         product.stock+=purchase["quantity"]
+        product.save()  #save in unutulmaması lazım!
         
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
@@ -65,3 +66,27 @@ class PurchasesView(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+        
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True) #! serializerdan geçirdikten sonra işlem yapıyoruz.
+        
+        #! #### UPDATE PRODUCT STOCK ####
+        purchase=request.data
+        product=Product.objects.get(id=instance.product_id)
+        
+        sonuc=purchase["quantity"] - instance.quantity
+        product.stock+=sonuc
+        product.save()
+        
+        
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)    
