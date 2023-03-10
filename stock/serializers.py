218a76471e63280e4b2514cdfc5a51070babcf44
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2023-03-10 11:45:32 +0300

    datetime,strftime

--- stock/serializers.py
+++ stock/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Category,Firm,Brand,Product,Purchases,Sales
+import datetime
 
 class CategorySerializer(serializers.ModelSerializer):
     product_count=serializers.SerializerMethodField() #bu method readonly, sadece get ile görüntülenir.
@@ -59,6 +60,22 @@ class PurchasesSerializer(serializers.ModelSerializer):
     product_id = serializers.IntegerField()
     brand_id = serializers.IntegerField()
     firm_id = serializers.IntegerField()
+    category=serializers.SerializerMethodField()
+    time_hour=serializers.SerializerMethodField()
+    createds=serializers.SerializerMethodField()
     class Meta:
         model=Purchases
-        fields=("id","user","user_id","firm","firm_id","brand","brand_id","product","product_id","quantity","price","price_total","createds","updated",)        
+        fields=("id","user","user_id","firm","firm_id","brand","brand_id","product","product_id","quantity","price","price_total","updated","category","time_hour","createds",)   
+        
+    # def get_category(self,obj):
+        # product=Product.objects.get(id=obj.product_id)
+        # return Category.objects.get(id=product.category_id).name  
+    # yukarıdaki fonksiyonun kısa yazımı
+    def get_category(self, obj):
+        return obj.product.category.name       
+        
+    def get_time_hour(self,obj):
+        return datetime.datetime.strftime(obj.createds,"%H:%M")    
+    
+    def get_createds(self,obj):
+        return datetime.datetime.strftime(obj.createds, "%d,%m,%Y")
